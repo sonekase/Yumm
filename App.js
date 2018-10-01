506ce83d57fbbcbf366fc9556045f2f35d3dc543
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-10-01 09:06:01 -0700

    Add linter-eslint to project.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import {Platform, Flatlist, StyleSheet, Text, View, Button} from 'react-native';
 import axios from 'axios';
 
 const config = {
-  headers: {'Authorization': 'Bearer API-KEY'},
+  headers: {'Authorization': 'Bearer GqXpqnEMa9EO9na_qp2XuDEDCuFYWGLJF-JlmK8qGm0oaTr-DsDxv65dBl-dsRL2aa6T9uNkY-fgyX1KdedpuWXZl9fXcfhr3MWLEvYmWWHBT4y-KofTCTKAG2WqW3Yx'},
   params: {
     term: 'pizza',
     location: 'Seattle',
@@ -11,20 +11,21 @@ const config = {
   }
 };
 
-export default class App extends Component {
+export default class S extends Component {
 
   componentWillMount() {
     axios.get('https://api.yelp.com/v3/businesses/search', config)
     .then(response => console.log(response));
     }
 
-  render(){
+  render() {
     return (
       <View style={styles.container}>
         <Text> My first yelp authentication request. </Text>
+        <Text> Return API data. </Text>
         <Button
           onPress={() => {
-            Alert.alert('You tapped the button!');
+            Alert.alert('You tapped the button!')
           }}
           title="Find Pizza"
         />

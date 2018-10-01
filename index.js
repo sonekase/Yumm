502fff83b5c853978b7744b5bb1711ed59df827a
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-10-01 09:16:56 -0700

    Modified library in index.js.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-/** @format */
+// Index.ios.js - place code in here for IOS!!!!
 
+// Import a library to help create a component
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Create a component
+
+
+// Render it to the device
+
 
 AppRegistry.registerComponent(appName, () => App);

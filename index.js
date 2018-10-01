f03a3d61e5c011d285ff147f1c7ed388c9668018
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-10-01 09:58:15 -0700

    Add header.js component.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 // Index.ios.js - place code in here for IOS!!!!
 
 // Import a library to help create a component
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
 import React from 'react';
-import ReactNative from 'react-native';
+import { Text, AppRegistry } from 'react-native';
 
 // Create a component
-
+const App = () => {
+  return (
+    <Text>Yummm! App</Text>
+  );
+};
 
 // Render it to the device
-
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('Yumm', () => App);
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+// Import libraries for making a compent
+import React from 'react';
+import { Text } from 'react-native';
+
+// Make a component
+const Header = () => {
+  return (
+    <Text>Restaurants!</Text>
+  );
+};
+
+// Make the component available to other parts of the app

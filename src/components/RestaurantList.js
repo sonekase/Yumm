4df16d898227111c40e0a59f11a643ecc8d9d59a
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-10-01 11:51:20 -0700

    Change RestaurantList to a class, added Axios to project and tested API request.

--- src/components/RestaurantList.js
+++ src/components/RestaurantList.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const RestaurantList = () => {
-  return (
-    <View>
-      <Text>Restaurant List!!!</Text>
-    </View>
-  );
+const config = {
+  headers: {'Authorization': 'Bearer GqXpqnEMa9EO9na_qp2XuDEDCuFYWGLJF-JlmK8qGm0oaTr-DsDxv65dBl-dsRL2aa6T9uNkY-fgyX1KdedpuWXZl9fXcfhr3MWLEvYmWWHBT4y-KofTCTKAG2WqW3Yx'},
+  params: {
+    term: 'pizza',
+    location: 'Seattle',
+    total: 10
+  }
 };
+
+class RestaurantList extends Component {
+  componentWillMount() {
+    axios.get('https://api.yelp.com/v3/businesses/search', config)
+    .then(response => console.log(response));
+    }
+
+  render() {
+    return (
+      <View>
+        <Text>Restaurant List!!!</Text>
+      </View>
+    );
+  }
+}
 
 export default RestaurantList;

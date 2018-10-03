4f6a791c6c4dd8af4087313c46e9a46582e4b126
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-10-03 13:37:37 -0700

    Wrap Card onto CardSection component to render restaurants.

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Text } from 'react-native';
 import Card from './Card';
+import CardSection from './CardSection';
 
 const RestaurantDetail = (props) => {
   return (
     <Card>
-      <Text>{props.restaurant.name}</Text>
+      <CardSection>
+        <Text>{props.restaurant.name}</Text>
+      </CardSection>
     </Card>
   );
 };

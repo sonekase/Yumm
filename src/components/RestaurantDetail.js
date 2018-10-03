360454ab57d65858b8e5a995166315c5e2024868
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-10-03 13:07:58 -0700

    Add style to Card Component.

--- src/components/RestaurantDetail.js
+++ src/components/RestaurantDetail.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+import Card from './Card';
 
 const RestaurantDetail = (props) => {
   return (
-    <View>
+    <Card>
       <Text>{props.restaurant.name}</Text>
-    </View>
+    </Card>
   );
 };
 
